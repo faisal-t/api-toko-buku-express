eab5cbd1721ecd58ec73052424cc07eaac709aaa
Faisal <fmahromi@gmail.com>
2022-03-06 21:34:20 +0700

    migration with sequelize

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var authRouter = require('./app/api/auth/router');
+var categoriesRouter = require('./app/api/categories/router');
+const URL = `/api/v1`
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req,res) => {
+    res.json({message: 'api express baru'});
+});
+app.use(`${URL}`, authRouter);
+app.use(`${URL}`, categoriesRouter);
+
+module.exports = app;
