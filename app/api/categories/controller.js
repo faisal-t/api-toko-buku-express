f5b934bd12d2a08d4f10a768e475867d4baf5a9e
Faisal <fmahromi@gmail.com>
2022-03-13 22:43:15 +0700

    add some feature to categories api

---
+++ app/api/categories/controller.js
@@ -0,0 +1,71 @@
+const { Category } = require('../../db/models');
+
+
+module.exports = {
+    getAllCategories: async (req, res, next) => {
+        try {
+            const categories = await Category.findAll({
+                where: { user: req.user.id },
+                attributes: ['id', 'name'],
+            });
+            res.status(200).json({
+                message: 'Success get All Categories',
+                data: categories,
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+    createCategories: async (req, res, next) => {
+        try {
+            const { name } = req.body;
+            const categories = await Category.create({
+                name: name,
+                user: req.user.id,
+            })
+
+            res.status(201).json({
+                message: 'success create categories',
+                data: categories,
+            })
+        } catch (err) {
+            next(err);
+        }
+    },
+    updateCategory: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const { name } = req.body;
+            const checkCategory = await Category.findOne({
+                where: {
+                    id: id,
+                    user: req.user.id,
+                }
+            });
+            const categories = await checkCategory.update({ name: name });
+
+
+            res.status(201).json({
+                message: 'success Update categories',
+                data: categories,
+            })
+        } catch (err) {
+            next(err)
+        }
+    },
+    deleteCategory: async (req, res, next) => {
+        Category.findOne({
+            where: { id: req.params.id, user: req.user.id },
+        }).then((categories) => {
+            if (categories) {
+                categories.destroy();
+                res.status(200).json({
+                    message: 'success delete categories',
+                    data: categories,
+                });
+
+            }
+        }).catch((err) => next(err));
+
+    }
+}
--- app/api/categories/router.js
+++ app/api/categories/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../../middleware/auth');
+const controller = require('./controller');
 
 /* GET home page. */
-router.get('/categories', auth, function (req, res) {
-  res.status(200).json({ message: 'router category' });
-});
+router.get('/categories', auth, controller.getAllCategories);
+router.post('/categories', auth, controller.createCategories);
+router.put('/categories/:id', auth, controller.updateCategory);
+router.delete('/categories/:id', auth, controller.deleteCategory);
+
 
 module.exports = router;
